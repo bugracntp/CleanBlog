1c93a372b873fbec4a78d72e46e46cc7a6888d8e
bugracntp <can123.bugra@gmail.com>
2023-01-25 16:20:33 +0300

    mongodb entegrated to project

--- app.js
+++ app.js
@@ -1,19 +1,38 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const ejs = require('ejs');
 const path = require('path');
 
+const Post = require('./models/posts') 
+
 const app = express();
 const port = 3000;
+
+
+// DB Connection
+mongoose.set("strictQuery", false);
+mongoose.connect('mongodb://localhost/cleanblog-test-db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
 
 // TAMPLATE ENGINE
 app.set('view engine', 'ejs');
 
 // MIDDLEWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true })); // url de ki datayı okumaya yarıyor
+app.use(express.json()); // url deki datayı json a çevirmeye yarıyor
+
 
 // ROUTERS
-app.get('/', (req, res) => {
-   res.render('index');
+app.get('/', async (req, res) => {
+   const Posts = await Post.find({})
+   res.render('index',{
+      Posts
+   });
 });
 
 app.get('/about', (req, res) => {
@@ -27,6 +46,11 @@ app.get('/post', (req, res) => {
 app.get('/addpost', (req, res) => {
    res.render('add_post');
 });
+
+app.post('/sendpost', async (req,res)=>{
+   await Post.create(req.body)
+   res.redirect('/'); // adresine yönlendirioruz
+})
 
 app.listen(port, () => {
    console.log(`sunucu ${port}'unda açıldı.`);
